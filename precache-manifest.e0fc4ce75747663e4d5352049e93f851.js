5d7047347ea565fb29697c3fc0addee4fd8a6a3c
Andriy Luchko <luchko@Andriys-iMac.local>
2019-07-18 17:52:31 +0300

    Updates

---
+++ precache-manifest.e0fc4ce75747663e4d5352049e93f851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e42444e190f8a512dcee5b47de90c08",
+    "url": "/store/index.html"
+  },
+  {
+    "revision": "9e052bd776ebda4f52df",
+    "url": "/store/static/css/main.8ed1c25e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/store/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9e052bd776ebda4f52df",
+    "url": "/store/static/js/main.11098608.chunk.js"
+  },
+  {
+    "revision": "4642218ef387c551e27b",
+    "url": "/store/static/js/runtime~main.852bf9be.js"
+  }
+]);

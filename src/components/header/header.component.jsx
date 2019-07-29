8bec9b0f1958b88130655844b47a79efc340e187
Andriy Luchko <luchko@Andriys-iMac.local>
2019-07-29 11:38:08 +0300

    added display name

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,24 +10,28 @@ const Header = ({ currentUser }) => {
       <div className="wrapper">
         <Link
           className="menu-item"
-          to="/">Store
+          to="/">
+          Gallery
         </Link>
 
-        {
-          currentUser ? (
-            <div
-              className="menu-item"
-              onClick={() => auth.signOut()}>
-              Log Out
-            </div>
+        <div className="user-data-container">
+          {currentUser ? (
+            <React.Fragment>
+              <span className="display-name">{currentUser.displayName}</span>
+              <div
+                className="menu-item"
+                onClick={() => auth.signOut()}>
+                Log Out
+              </div>
+            </React.Fragment>
           ) : (
             <Link
               className="menu-item"
               to="/signin">
               Log In
             </Link>
-          )
-        }
+          )}
+        </div>
       </div>
     </header>
   );

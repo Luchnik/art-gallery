01d833f8aa123986274a02c4e1acbbf55f2a379a
Andriy Luchko <luchko@Andriys-iMac.local>
2019-07-30 10:47:46 +0300

    Updates

---
+++ precache-manifest.cb6e29d2a5d3e2ed6f1a039ca0f625ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca366b370b5313c270d2fd438271045",
+    "url": "/art-gallery/index.html"
+  },
+  {
+    "revision": "a57d9bd1f42778059c27",
+    "url": "/art-gallery/static/css/main.d17899b5.chunk.css"
+  },
+  {
+    "revision": "2a25835537d8790482fc",
+    "url": "/art-gallery/static/js/2.4652cb55.chunk.js"
+  },
+  {
+    "revision": "a57d9bd1f42778059c27",
+    "url": "/art-gallery/static/js/main.672b0f07.chunk.js"
+  },
+  {
+    "revision": "35a87c5a2af526076011",
+    "url": "/art-gallery/static/js/runtime~main.4e2534c9.js"
+  },
+  {
+    "revision": "1d8e929fdd4922b0dba30dd465e3834d",
+    "url": "/art-gallery/static/media/jumbotron.1d8e929f.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/art-gallery/precache-manifest.01ac4e93f52c35a960b785212f4e664e.js"
+  "/art-gallery/precache-manifest.cb6e29d2a5d3e2ed6f1a039ca0f625ef.js"
 );
 
 self.addEventListener('message', (event) => {
